ac3524aa9d0d3cd1c5118201410a4f37e1973635
zhh <nkuzjh@gmail.com>
2025-08-26 20:00:45 +0800

    exp9 img_aug coding

--- tta.py
+++ tta.py
@@ -214,12 +214,10 @@ def main(args, config):
         max_epoch = config['schedular']['epochs']
         for epoch in range(0, max_epoch):
 
-            sims_matrix_t2i, image_embeds, text_embeds, text_atts = evaluation_itc(
-                model, test_loader, tokenizer, device, config)
-            train_stats = test_time_adapt_itm(model, optimizer, scaler, epoch, device, lr_scheduler, config, tta_loader)#sims_matrix_t2i, image_embeds, text_embeds, text_atts)
+            # sims_matrix_t2i, image_embeds, text_embeds, text_atts = evaluation_itc(model, test_loader, tokenizer, device, config)
+            train_stats = test_time_adapt_itm(model, optimizer, scaler, epoch, device, lr_scheduler, config, sims_matrix_t2i, image_embeds, text_embeds, text_atts)
 
-            sims_matrix_t2i, image_embeds, text_embeds, text_atts = evaluation_itc(
-                model, test_loader, tokenizer, device, config)
+            # sims_matrix_t2i, image_embeds, text_embeds, text_atts = evaluation_itc(model, test_loader, tokenizer, device, config)
             score_test_t2i = evaluation_itm(
                 model,
                 device, config, args,
